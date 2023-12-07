da0ea1e5faa5c7e0ade89ab316edbc4047965d27
Nikita <nik.porshennikov@mail.ru>
2023-12-08 01:49:51 +0700

    initial commit

---
+++ logging-starter/src/main/java/com/synthilearn/loggingstarter/LogingStarterApplication.java
@@ -0,0 +1,13 @@
+package com.synthilearn.loggingstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogingStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogingStarterApplication.class, args);
+    }
+
+}
